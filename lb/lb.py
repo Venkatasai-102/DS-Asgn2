386deb63cedabcd18e0a12c22fe52c9564e52514
bhanuteja <bhanutejasiripuram468@gmail.com>
2024-03-13 02:54:52 +0530

    added code for /add endpoint

--- lb/lb.py
+++ lb/lb.py
@@ -136,29 +136,63 @@ async def init_system(request: Request):
                 print("Issue is here ):")
 
         
-# @app.post("/add")
-# async def add_servers(request: Request):
-#     req = await request.json()
-#     n = req['N']
-#     new_shards, servers = req['shards'], req['servers']
+@app.post("/add")
+async def add_servers(request: Request):
+    req = await request.json()
+    n = req['n']
+    new_shards, servers = req['new_shards'], req['servers']
 
-#     shard_names = []
-#     for shard in new_shards:
-#         shard_names.append(shard['Shard_id'])
+    if n > len(servers):
+        return JSONResponse(
+            status_code=400,
+            content={
+                "message": "<Error> Number of new servers (n) is greater than newly added instances",
+                "status": "failure"
+            }
+        )
     
-#     for server_name in servers:
-#         result = create_server(server_name)
+    # need to create random server id when not given, currently assuming n = len(servers)
+    ip = {}
+    for server_name in servers:
+        name , ipaddr = create_server(name=server_name)
+        print("Successfully created ",name)
+        ip[name] = ipaddr
+    
+    for server_name in servers:
+        # on success, adding this new server to the respective shards' consistent hashing object
+        app.server_list[server_name] = {"index": randint(1, MAX_SERVER_INDEX), "ip": ip[server_name]}
+        for sh in servers[server_name]:
+            if sh not in app.hash_dict:
+                app.hash_dict[sh] = ConsistentHashing(NUM_SLOTS, VIR_SERVERS)
+            
+#             app.hash_dict[sh].add_server(app.server_list[server_name]['index'], result['ipaddr'], 8000)
+
+            # need to handle the case when one of the server fails, we need to stop the already created servers or do something else
+            # adding entry in MapT
+
+            add_mapt_query = "INSERT INTO MapT VALUES (%s,%s)"
+            mysql_cursor.execute(add_mapt_query,(sh,server_name))
+            mysql_conn.commit()
         
-#         if result['status'] == 'failure':
-#             return result['response']
-            
-#         # on success
-#         app.server_list[server_name] = {"index": randint(1, MAX_SERVER_INDEX), "ip": result['ipaddr']}
-#         for sh in servers[server_name]:
-#             if sh not in app.hash_dict:
-#                 app.hash_dict[sh] = ConsistentHashing(NUM_SLOTS, VIR_SERVERS)
-            
-#             app.hash_dict[sh].add_server(app.server_list[server_name]['index'], result['ipaddr'], 8080)
+        # calling the /config api of this new server
+        url = f"http://{ip[server_name]}:{8000}/config"
+        data = {
+            "schema": {"columns":["Stud_id","Stud_name","Stud_marks"],
+                       "dtypes":["Number","String","String"]},
+            "shards": servers[server_name]
+        }
+        print(data)
+        while True:
+            try:
+                requests.post(url, json=data)
+                break
+            except:
+                print("retry after sleeping 30sec")
+                time.sleep(30)
+                
 
-
-# uvicorn.run(app, host="0.0.0.0", port=8080)
+        
+    for shard in new_shards:
+        shard_query ="INSERT INTO ShardT VALUES (%s,%s,%s,%s)"
+        mysql_cursor.execute(shard_query,(shard["Stud_id_low"],shard["Shard_id"],shard["Shard_size"],shard["Stud_id_low"]))
+        mysql_conn.commit()
