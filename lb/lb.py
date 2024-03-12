89db077a2e93806bdbdc0fc82092bbbbd07df65c
Venkatasai-102 <venkatasaikgp102@gmail.com>
2024-03-12 17:33:28 +0530

    Changes: - updated metadb.env file - included server in docker-compose file - added /read api `without locks` in lb.py

--- lb/lb.py
+++ lb/lb.py
@@ -5,10 +5,11 @@ import os
 import time
 from consistent_hashing import ConsistentHashing
 import requests
-
+import asyncio
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse,RedirectResponse
 import mysql.connector as conn
+from helpers import create_server, remove_servers, get_servers
 
 print("Strating Load Balancer......")
 
@@ -42,41 +43,9 @@ VIR_SERVERS = 9
 # maps for storing locally for loadbalancer
 app.hash_dict = {}
 app.server_list = {}
+app.read_write_lock_dict = {}
+app.read_semaphore_dict = {}
 
-
-def create_server(server_name):
-    command = "docker run --name {container_name} --env SERVER_ID={container_name} \
-           -d --network={network_name} serverimg".format(container_name=server_name, network_name="mynet")
-
-    result = subprocess.run(command, shell=True, text=True)
-    print(result.returncode)
-    
-    if result.returncode == 0:
-        ipcommand = [
-            'docker',
-            'inspect',
-            '--format={{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}',
-            server_name
-        ]
-        ip = subprocess.run(
-            ipcommand, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True, text=True)
-        ipaddr = ip.stdout.strip()        
-        return {'status': 'success', 'ipaddr': ipaddr, 'name': server_name }
-    
-    else:
-        return {'status': 'failure',
-                'response': JSONResponse(
-            status_code=500,
-            content={
-                "message": {
-                    "N": len(list(app.server_list.keys())),
-                    "replicas": list(app.server_list.keys()),
-                    "error": f"failed to create server: {server_name}"
-                },
-                "status": "failure"
-            }
-            )
-        }
 
 @app.post("/init")
 async def init_system(request: Request):
@@ -138,12 +107,14 @@ async def init_system(request: Request):
                pass
             
         # on success
-        app.serverList[server_name] = {"index": randint(1, MAX_SERVER_INDEX), "ip": result['ipaddr']}
+        app.server_list[server_name] = {"index": randint(1, MAX_SERVER_INDEX), "ip": result['ipaddr']}
         for sh in servers[server_name]:
             if sh not in app.hash_dict:
                 app.hash_dict[sh] = ConsistentHashing(NUM_SLOTS, VIR_SERVERS)
+                app.read_write_lock_dict[sh] = asyncio.Lock()
+                app.read_semaphore_dict[sh] = asyncio.Semaphore(2)
             
-            app.hash_dict[sh].add_server(app.serverList[server_name]['index'], result['ipaddr'], 8000)
+            app.hash_dict[sh].add_server(app.server_list[server_name]['index'], result['ipaddr'], 8000)
             
             # need to handle the case when one of the server fails, we need to stop the already created servers or do something else
             ## add shard-server mapping to database
@@ -184,12 +155,14 @@ async def add_servers(request: Request):
             return result['response']
             
         # on success, adding this new server to the respective shards' consistent hashing object
-        app.serverList[server_name] = {"index": randint(1, MAX_SERVER_INDEX), "ip": result['ipaddr']}
+        app.server_list[server_name] = {"index": randint(1, MAX_SERVER_INDEX), "ip": result['ipaddr']}
         for sh in servers[server_name]:
             if sh not in app.hash_dict:
                 app.hash_dict[sh] = ConsistentHashing(NUM_SLOTS, VIR_SERVERS)
+                app.read_write_lock_dict[sh] = asyncio.Lock()
+                app.read_semaphore_dict[sh] = asyncio.Semaphore(2)
             
-#             app.hash_dict[sh].add_server(app.serverList[server_name]['index'], result['ipaddr'], 8000)
+            app.hash_dict[sh].add_server(app.server_list[server_name]['index'], result['ipaddr'], 8000)
 
             # need to handle the case when one of the server fails, we need to stop the already created servers or do something else
             # adding entry in MapT
@@ -214,4 +187,58 @@ async def add_servers(request: Request):
         shard_query = f'INSERT INTO ShardT VALUES ({shard["Stud_id_low"]}, {shard["Shard_id"]}, {shard["Shard_size"]}, {shard["Stud_id_low"]})'
         mysql_cursor.execute(shard_query)
         mysql_conn.commit()
-uvicorn.run(app, host="0.0.0.0", port=8080)
+
+@app.get("/read")
+async def read_data(request: Request):
+    req = await request.json()
+    stud_low = req["Stud_id"]["low"]
+    stud_high = req["Stud_id"]["high"]
+
+    # need to optimize to selectively get the shard entries instead of getting all shards in ShardT
+    get_shards_query = "SELECT * from ShardT"
+    mysql_cursor.execute(get_shards_query)
+    response = mysql_cursor.fetchall()
+    request_id = randint()%NUM_SLOTS
+
+    shards_queried = []
+
+    valid_rows = []
+    for row in response:
+        if not (stud_low > row[0]+row[3] or row[0] > stud_high):
+            valid_rows.append(row)
+            shards_queried.append(row[1])
+
+    result = []
+
+    for row in valid_rows:
+        consistent_hashing_object = app.hash_dict[row[1]]
+        server = consistent_hashing_object.get_nearest_server(request_id)
+
+        url = f"{server.server_ip}:{8000}/read"
+        data = {
+            "low": max(row[0], stud_low),
+            "high": min(row[0]+row[3], stud_high)
+        }
+
+        resp = request.get(url, data)
+        if resp["status"] == "success":
+            result += resp["data"]
+        else:
+            return JSONResponse(
+            status_code=400,
+            content={
+                "message": "Invalid query",
+                "status": "failure"
+            }
+        )
+    
+    return {
+        "shards_queried": shards_queried,
+        "data": result,
+        "status": "success"
+    }
+
+    
+# Run the FastAPI app
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
