2104a6d5724b29a48d5be54d12c464f99ce2a446
Venkatasai-102 <venkatasaikgp102@gmail.com>
2024-03-13 11:13:21 +0530

    Changes: - Added return statements to /init and /add api Todo: - Need to test /read api

--- lb/lb.py
+++ lb/lb.py
@@ -1,6 +1,5 @@
 from random import randint
 import uvicorn
-import subprocess
 import os
 import time
 from consistent_hashing import ConsistentHashing
@@ -11,14 +10,14 @@ from fastapi.responses import JSONResponse,RedirectResponse
 import mysql.connector as conn
 from helpers import create_server, remove_servers, get_servers
 
-print("Strating Load Balancer......")
+print("Starting Load Balancer......")
 
 while True:
     try:
         mysql_conn = conn.connect(
             host="metadb",
-            user=os.getenv("MYSQL_USER", "bhanu"),
-            password=os.getenv("MYSQL_PASSWORD", "bhanu@1489"),
+            user=os.getenv("MYSQL_USER", "user"),
+            password=os.getenv("MYSQL_PASSWORD", "Pass@123"),
             database=os.getenv("MYSQL_DATABASE", "StudentDB"),
         )
         print("connected")
@@ -43,6 +42,8 @@ VIR_SERVERS = 9
 # maps for storing locally for loadbalancer
 app.hash_dict = {}
 app.server_list = {}
+
+# locks and semaphores for handling concurrency
 app.read_write_lock_dict = {}
 app.read_semaphore_dict = {}
 
@@ -52,7 +53,8 @@ async def init_system(request: Request):
     req = await request.json()
     n, schema = req['N'], req['schema']
     shards, servers = req['shards'], req['servers']
-    
+    print(servers)
+
     if n != len(shards) or n != len(servers):
         return JSONResponse(
             status_code=400,
@@ -85,57 +87,69 @@ async def init_system(request: Request):
     mysql_cursor.execute(create_shardt_query)
     mysql_conn.commit()
     
+    ip={}
     for server_name in servers:
-        result = create_server(server_name)
-        if result['status'] == 'failure':
-            return result['response']
-
-        # need to change to the network name currently put as localhost for testing
-        url = f"http://{result['ipaddr']}:{8000}/config"
+        name,ipaddr = create_server(name=server_name)
+        ip[name] = ipaddr
+    
+    for server_name in servers:
+        url = f"http://{ip[server_name]}:{8000}/config"
         print(url)
         data = {
             "schema": schema,
             "shards": [sh["Shard_id"] for sh in shards]
         }
+        print(data)
         # time.sleep(10)
         while True:
            try:
-                requests.post(url, data,timeout=None)
+                result = requests.post(url, json=data,timeout=None)
+                print(result.ok)
                 break
-           except Exception as e:
-                # print("trying again")
-               pass
+           except requests.RequestException as e:
+                print("trying again")
+                time.sleep(30)
             
         # on success
-        app.server_list[server_name] = {"index": randint(1, MAX_SERVER_INDEX), "ip": result['ipaddr']}
+        app.server_list[server_name] = {"index": randint(1, MAX_SERVER_INDEX), "ip": ip[server_name]}
         for sh in servers[server_name]:
             if sh not in app.hash_dict:
                 app.hash_dict[sh] = ConsistentHashing(NUM_SLOTS, VIR_SERVERS)
-                app.read_write_lock_dict[sh] = asyncio.Lock()
-                app.read_semaphore_dict[sh] = asyncio.Semaphore(2)
             
-            app.hash_dict[sh].add_server(app.server_list[server_name]['index'], result['ipaddr'], 8000)
+            app.hash_dict[sh].add_server(app.server_list[server_name]['index'], ip[server_name], 8000)
             
-            # need to handle the case when one of the server fails, we need to stop the already created servers or do something else
             ## add shard-server mapping to database
-            add_mapt_query = f"""
-            INSERT INTO MapT VALUES ({sh}, {server_name})
-            """
-            mysql_cursor.execute(add_mapt_query)
-            mysql_conn.commit()
+            add_mapt_query = "INSERT INTO MapT VALUES (%s, %s)"
+            print(add_mapt_query)
+            try:
+                mysql_cursor.execute(add_mapt_query,(sh,server_name))
+                mysql_conn.commit()
+            except Exception as e:
+                print(e)
+                print("Issue is here")
 
     # creating all shard entries in ShardT
     for shard in shards:
-        shard_query = f'INSERT INTO ShardT VALUES ({shard["Stud_id_low"]}, {shard["Shard_id"]}, {shard["Shard_size"]}, {shard["Stud_id_low"]})'
-        mysql_cursor.execute(shard_query)
-        mysql_conn.commit()
+        shard_query = "INSERT INTO ShardT VALUES (%s,%s,%s,%s)"
 
-  
+        try:
+            mysql_cursor.execute(shard_query,(shard["Stud_id_low"],shard["Shard_id"],shard["Shard_size"],shard["Stud_id_low"]))
+            mysql_conn.commit()
+        except Exception as e:
+                print(e)
+                print("Issue is here ):")
+            
+    return {
+        "message" : "Configured Database",
+        "status" : "success"
+    }
+
+        
 @app.post("/add")
 async def add_servers(request: Request):
     req = await request.json()
     n = req['n']
-    new_shards, servers = req['shards'], req['servers']
+    new_shards, servers = req['new_shards'], req['servers']
 
     if n > len(servers):
         return JSONResponse(
@@ -147,46 +161,59 @@ async def add_servers(request: Request):
         )
     
     # need to create random server id when not given, currently assuming n = len(servers)
-
+    ip = {}
     for server_name in servers:
-        result = create_server(server_name)
-        
-        if result['status'] == 'failure':
-            return result['response']
-            
+        name , ipaddr = create_server(name=server_name)
+        print("Successfully created ",name)
+        ip[name] = ipaddr
+    
+    message = "Added"
+    
+    for server_name in servers:
         # on success, adding this new server to the respective shards' consistent hashing object
-        app.server_list[server_name] = {"index": randint(1, MAX_SERVER_INDEX), "ip": result['ipaddr']}
+        app.server_list[server_name] = {"index": randint(1, MAX_SERVER_INDEX), "ip": ip[server_name]}
         for sh in servers[server_name]:
+            message += f" {server_name},"
             if sh not in app.hash_dict:
                 app.hash_dict[sh] = ConsistentHashing(NUM_SLOTS, VIR_SERVERS)
-                app.read_write_lock_dict[sh] = asyncio.Lock()
-                app.read_semaphore_dict[sh] = asyncio.Semaphore(2)
             
-            app.hash_dict[sh].add_server(app.server_list[server_name]['index'], result['ipaddr'], 8000)
+            app.hash_dict[sh].add_server(app.server_list[server_name]['index'], ip[server_name], 8000)
 
             # need to handle the case when one of the server fails, we need to stop the already created servers or do something else
             # adding entry in MapT
-            add_mapt_query = f"""
-            INSERT INTO MapT VALUES ({sh}, {server_name})
-            """
-            mysql_cursor.execute(add_mapt_query)
+
+            add_mapt_query = "INSERT INTO MapT VALUES (%s,%s)"
+            mysql_cursor.execute(add_mapt_query,(sh,server_name))
             mysql_conn.commit()
         
         # calling the /config api of this new server
-        url = f"http://{result['ipaddr']}:{8000}/config"
+        url = f"http://{ip[server_name]}:{8000}/config"
         data = {
             "schema": {"columns":["Stud_id","Stud_name","Stud_marks"],
                        "dtypes":["Number","String","String"]},
-            "shards": [sh["Shard_id"] for sh in servers[server_name]]
+            "shards": servers[server_name]
         }
-
-        requests.post(url, data)
-    
+        print(data)
+        while True:
+            try:
+                requests.post(url, json=data)
+                break
+            except:
+                print("retry after sleeping 30sec")
+                time.sleep(30)
+        
     # adding entries in ShardT
     for shard in new_shards:
-        shard_query = f'INSERT INTO ShardT VALUES ({shard["Stud_id_low"]}, {shard["Shard_id"]}, {shard["Shard_size"]}, {shard["Stud_id_low"]})'
-        mysql_cursor.execute(shard_query)
+        shard_query ="INSERT INTO ShardT VALUES (%s,%s,%s,%s)"
+        mysql_cursor.execute(shard_query,(shard["Stud_id_low"],shard["Shard_id"],shard["Shard_size"],shard["Stud_id_low"]))
         mysql_conn.commit()
+
+
+    return {
+        "N": len(app.server_list),
+        "message": message,
+        "status": "success"
+    }
 
 @app.get("/read")
 async def read_data(request: Request):
@@ -220,7 +247,7 @@ async def read_data(request: Request):
             "high": min(row[0]+row[3], stud_high)
         }
 
-        resp = request.get(url, data)
+        resp = requests.get(url, json=data)
         if resp["status"] == "success":
             result += resp["data"]
         else:
