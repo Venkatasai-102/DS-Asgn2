6190b22426430366c50c82d626526220608d0dcb
Venkatasai-102 <venkatasaikgp102@gmail.com>
2024-03-14 20:01:17 +0530

    Changes: - Added /write and /update api in loadbalancer

--- lb/lb.py
+++ lb/lb.py
@@ -408,7 +408,109 @@ async def rm_servers(request: Request):
         print("Exception:", e)
         return JSONResponse(status_code=500, content={"message": "Unexpected error", "status": "failure"})
 
-    
+
+@app.post("/write")
+async def write(request: Request):
+    # need to map shard to server
+    try:
+        req = await request.json()
+        students = req["data"]
+        print(students)
+
+        GET_SHARDS_QUERY = "SELECT * FROM ShardT"
+        mysql_cursor.execute(GET_SHARDS_QUERY)
+        rows = mysql_cursor.fetchall()
+        shards = {row[1]: {"students":[],"attr":list(row),"server":[]} for row in rows}
+        
+        mysql_cursor.execute("SELECT * FROM MapT")
+        MapT_rows =mysql_cursor.fetchall()
+        
+        for MapT_row in MapT_rows:
+            shard_id = MapT_row[0]
+            server = MapT_row[1]
+            shards[shard_id]["server"].append(server)
+
+        print(shards)
+        for student in students:
+            Stud_id =student["Stud_id"]
+            Stud_name=student["Stud_name"]
+            Stud_marks=student["Stud_marks"]
+            
+            for shard_id in shards:
+                if shards[shard_id]["attr"][0] <= Stud_id and Stud_id <= shards[shard_id]["attr"][2]:
+                    shards[shard_id]["students"].append((Stud_id,Stud_name,Stud_marks))
+        
+        for shard_id in shards:
+            # acquire the lock for this shard
+
+            queries = [{"Stud_id":stud[0],"Stud_name":stud[1],"Stud_marks":stud[2]} for stud in shards[shard_id]["students"]]
+            data= { "shard":shard_id,"curr_idx":shards[shard_id]["attr"][3] ,"data":queries}
+            curr_idx = None
+            for server in shards[shard_id]["server"]:
+                print(f"Sending request to {server} :{shard_id}")
+                result = requests.post(f"http://{server}:8000/write",json=data,timeout=15)
+                print(result.json())
+                curr_idx = result.json()["current_idx"]
+            shards[shard_id]["attr"][3]=curr_idx
+            mysql_cursor.execute("UPDATE ShardT SET valid_idx= ? WHERE Stud_id_low = ? AND Shard_id = ?",(curr_idx,shards[shard_id]["attr"][0],shard_id))
+            mysql_conn.commit()
+        
+        return {"message":f"{len(students)} Data entries added","status":"success"}
+        
+    except RequestException as e:
+        print("RequestException:", e)
+        return JSONResponse(status_code=500, content={"message": "Request failed", "status": "failure"})
+        
+    except sqlite3.Error as e: 
+        print("SQLite Error:", e)
+        return JSONResponse(status_code=500, content={"message": "SQLite error", "status": "failure"})
+        
+    except Exception as e:
+        print("Other Exception:", e)
+        return JSONResponse(status_code=500, content={"message": "Unexpected error", "status": "failure"})
+        
+
+@app.put("/update")
+async def update_shard(request: Request):
+    req = await request.json()
+    Stud_id = req["Stud_id"]
+    Student = req["data"]
+
+    mysql_cursor.execute("SELECT DISTINCT Shard_id FROM ShardT  WHERE Stud_id_low <= ? AND Stud_id_low + Shard_size > ?",(Stud_id,Stud_id))
+    result = mysql_cursor.fetchone()
+    print(result)
+    if result:
+        shard_id = result[0]
+        
+        #get lock on the shard
+
+        #get all servers that contain the shard
+        mysql_cursor.execute("SELECT DISTINCT Server_id FROM MapT WHERE Shard_id = ?",(shard_id,))
+        servers = mysql_cursor.fetchall()
+        print(servers)
+        if servers:
+            for server in servers:
+                server = server[0]
+                payload = {
+                    "shard":shard_id,
+                    "Stud_id":Stud_id,
+                    "data":Student
+                }
+                result = requests.put(f"http://{server}:8000/update",json=payload,timeout=15)
+                
+                if not result.ok:
+                    raise HTTPException(status_code=500,detail="Internal error")
+            
+            return {
+                "message": f"Data entry for Stud_id:{Stud_id} updated",
+                "status" : "success"
+            }
+            
+        else:
+            return {
+                "message" : "No server found",
+                "status"  : "failure"
+            }
     
 # Run the FastAPI app
 if __name__ == "__main__":
