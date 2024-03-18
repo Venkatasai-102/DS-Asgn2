d798358dad438c5fece1a5c5c35a1179e76025f8
bhanuteja <bhanutejasiripuram@kgpian.iitkgp.ac.in>
2024-03-18 17:45:46 +0530

    add code for updating the database when respawning the server

--- lb/crash_handler.py
+++ lb/crash_handler.py
@@ -4,6 +4,9 @@ from requests.adapters import HTTPAdapter, Retry
 
 from globals import app
 from helpers import create_server
+import sqlite3
+import requests
+
 
 def is_alive(server):
     print(f"Checking health of {server} ........")
@@ -67,18 +70,47 @@ def respawn_dead_server(dead_server):
     print(f"New server {new_server} is created with IP {ipaddr}!")
     
     print(f"Copying shard data from other servers ....... ", end=" ")
-    
+    print(shards,flush=True)
     for sh in shards:
         # changing the hash info
+        print(f".....Restoring {sh} ", flush=True)
         app.hash_dict[sh].remove_server(index)
         app.hash_dict[sh].add_server(index, ipaddr, 8000)
         
+
+        conn = sqlite3.connect('example.db')
+        cursor = conn.cursor()
         # Remove the shard - old server mapping from database       #TODO
-        
-        
+        cursor.execute("DELETE FROM MapT WHERE Server_id=? AND Shard_id=?",(dead_server,sh))
+        conn.commit()
         # get another server containing this shard from database & copy from that server       #TODO
-    
-    
+        cursor.execute("SELECT Server_id FROM MapT WHERE Shard_id=?",(sh,))
+        server_id = cursor.fetchone()
+        server_id = server_id[0]
+
+        # now make a req to    /copy endpoint
+        resp = requests.get(f"http://{ipaddr}:8000/copy",json={
+            "shards": [sh]
+        },timeout=15)
+        students = resp.json()[sh]
+        
+        #copy the shard data to the newly spawned server 
+        requests.post(f"http://{ipaddr}:8000/write",json={
+            "shard":sh,
+            "curr_idx": 0, 
+            "data": students
+        },timeout=15)
+        print(f"Successfully copied shard:{sh} data from ", server_id," to ", new_server, flush=True) 
+        # add the shard - new server mapping to database
+        cursor.execute("INSERT INTO MapT VALUES(?,?)",(sh,new_server))
+        conn.commit()
+        print(f"Successfully inserted shard:{sh} to server:{new_server} mapping into MapT", flush=True )
+        # Cleanup resources
+        cursor.close()
+        conn.close()
+
+
+    print("Done!", flush=True)
     
     
     
@@ -92,10 +124,11 @@ def check_server_health():
         server_names = list(app.server_list.keys())
         for server in server_names:
             if not is_alive(server):
+                print(app.server_list)
                 respawn_dead_server(server)
         # Unlock server_list
         
-        sleep(15)
+        sleep(1)
             
             
             
